c107e1a69f01c5508fcc4379e1f533e1243d7442
Nikita Chebykin <nikita@chebyk.in>
2013-11-25 15:13:27 +0400

    update independent files

--- config.js
+++ config.js
@@ -3,12 +3,23 @@ var config = {
     development: {
         online_check_interval: 1500,
         websocket_port: 10013,
-        redis_port: 6390
+        redis_port: 6390,
+        billing: {
+            hostname: 'http://dev.bestuabrides.com/chat_api',
+            port: 80,
+            timeout: 1000
+        }
     },
     test: {
         online_check_interval: 1500,
         websocket_port: 10013,
-        redis_port: 6390
+        redis_port: 6390,
+        billing: {
+            hostname: '127.0.0.1',
+            path: '/fake_api',
+            port: 12345,
+            timeout: 1000
+        }
     },
     production: {
         online_check_interval: 3500,
