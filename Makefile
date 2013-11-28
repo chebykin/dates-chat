b0a7a199aac32651deebef88b756f952c1ae0f4f
Nikita Chebykin <nikita@chebyk.in>
2013-11-28 12:52:43 +0400

    add Tracker and some tests

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 TESTS = test/*.js
 
 test:
+	redis-cli -p 6391 shutdown &
 	redis-server --port 6391 &
 	@NODE_ENV=test ./node_modules/.bin/mocha --harmony --require test/common
 	redis-cli -p 6391 flushall
---
+++ test/tracker_test.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var Tracker = require('../lib/tracker');
+
+describe('Tracker', function () {
+    beforeEach(function () {
+        this.tracker = new Tracker();
+    });
+
+    it('should emmit "start" when starting', function (done) {
+        this.tracker.on('start', function () {
+            done();
+        });
+        this.tracker.start();
+    });
+
+    describe('timers', function () {
+        beforeEach(function () {
+            this.clock = sinon.useFakeTimers();
+        });
+
+        afterEach(function () {
+            this.clock.restore();
+        });
+
+        it('should emmit tick every minute since start', function () {
+            var spy = sinon.spy();
+
+            this.tracker.on('tick', spy);
+            this.tracker.start();
+            this.clock.tick(190000);
+
+            spy.should.have.been.calledThrice;
+        });
+    });
+
+
+
+});
