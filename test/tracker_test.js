4adfd8165682a99aa953bb5f1d9158912b7fd479
Nikita Chebykin <nikita@chebyk.in>
2013-11-30 04:10:19 +0400

    add remove inactive timeout ability to Tracker

--- test/tracker_test.js
+++ test/tracker_test.js
@@ -36,12 +36,31 @@ describe('Tracker', function () {
         it('should be able remove remove manual off callback', function () {
             var spy = sinon.spy();
 
+            this.tracker.start();
             this.tracker.on('manual_off_timeout', spy);
             this.tracker.start_manual_off_timeout();
             this.tracker.remove_manual_off_timeout();
             this.clock.tick(config.timeouts.manual_off_timeout + 100);
 
             spy.should.not.have.been.called;
+        });
+
+        it('should be able to remove inactive timeout', function (done) {
+            var spy = sinon.spy(),
+                test = this;
+
+            this.tracker.on('inactive_timeout', spy);
+            this.tracker.start();
+            expect(this.tracker.inactive_timeout).to.not.equal(null);
+            this.tracker.remove_inactive_timeout();
+
+            setTimeout(function () {
+                expect(test.tracker.inactive_timeout).to.equal(null);
+                spy.should.not.have.been.called;
+                done();
+            }, 1);
+
+            this.clock.tick(config.timeouts.inactive_timeout + 1000);
         });
     });
 });
